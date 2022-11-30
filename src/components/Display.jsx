9fa1d263db61e51581de7c38bd0541f37d8d7dce
Rob Sassack <rsassack25@gmail.com>
2022-11-30 01:20:04 -0500

    Finished styling for full document

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -6,11 +6,13 @@ class Display extends React.Component {
     return this.props.info.education.map((education) => {
       return (
         <div className='display--education' key={nanoid()}>
-          <h3>{education.school}</h3>
+          <div className='display--apart'>
+            <h3>{education.school}</h3>
+            <p>{education.location}</p>
+          </div>
           <p>
             {education.degree} in {education.major}
           </p>
-          <p>{education.location}</p>
           <p>
             {education.start} - {education.end}
           </p>
@@ -23,12 +25,16 @@ class Display extends React.Component {
     return this.props.info.experience.map((experience) => {
       return (
         <div className='display--experience' key={nanoid()}>
-          <h3>{experience.company}</h3>
-          <p>{experience.title}</p>
-          <p>{experience.location}</p>
-          <p>
-            {experience.start} - {experience.end}
-          </p>
+          <div className='display--apart'>
+            <h3>{experience.company}</h3>
+            <p>{experience.location}</p>
+          </div>
+          <div className='display--apart'>
+            <p>{experience.title}</p>
+            <p>
+              {experience.start} - {experience.end}
+            </p>
+          </div>
           <p className='display--exp-desc'>{experience.description}</p>
         </div>
       );
@@ -39,14 +45,19 @@ class Display extends React.Component {
     return (
       <div className='display'>
         <div className='display--info'>
-          <h2>
-            {this.props.info.firstName} {this.props.info.lastName}
-          </h2>
-          <p>{this.props.info.location}</p>
-          <p>{this.props.info.email}</p>
-          <p>{this.props.info.phone}</p>
+          <div className='display--general'>
+            <h2>
+              {this.props.info.firstName} {this.props.info.lastName}
+            </h2>
+            <p>{this.props.info.location}</p>
+            <p>
+              {this.props.info.phone} | {this.props.info.email}
+            </p>
+          </div>
         </div>
+        <p className='display--title'>EDUCATION</p>
         {this.displayEducation()}
+        <p className='display--title'>EXPERIENCE</p>
         {this.displayExperience()}
       </div>
     );
